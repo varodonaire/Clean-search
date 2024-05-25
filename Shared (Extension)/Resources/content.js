df02b88a23b631113877adebe0b15c8fa120184d
Alvaro Mattus-Donaire <varodonaire@icloud.com>
2024-05-25 12:25:21 -0400

    Commit inicial

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,7 +1,9 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
-
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
+(function() {
+  const url = new URL(window.location.href);
+  if (url.hostname === 'www.google.com' && url.pathname === '/search') {
+    if (!url.searchParams.has('udm')) {
+      url.searchParams.append('udm', '14');
+      window.location.replace(url.toString());
+    }
+  }
+})();
